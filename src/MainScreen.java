1bcf0f88c583539335adc17e735da0ce60fb0429
Aidan Newell <am.newell@verizon.net>
2016-12-19 13:04:21 -0500

    Do da do

---
+++ src/MainScreen.java
@@ -0,0 +1,15 @@
+
+public class MainScreen extends FlexiblePictureExplorer{
+
+	public MainScreen(DigitalPicture picture) {
+		super(picture);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void mouseClickedAction(DigitalPicture pict, Pixel pix) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
