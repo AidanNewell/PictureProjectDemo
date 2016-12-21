7a70b442b0b2d142ffbe52431e1bd253c8d4c73d
Aidan Newell <am.newell@verizon.net>
2016-12-21 13:01:03 -0500

    Here

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -1,6 +1,16 @@
 
 public class MainScreen extends FlexiblePictureExplorer{
 
+	private String path = "slideshow/";
+	//This is used so I don't have to type it out every time in the three dimensional array
+	
+	
+	private String[][][] images = new String[][][] {{{},{},{}},
+										 	 	   {{},{},{}},
+										 	 	   {{},{},{}}};
+	
+	
+	
 	public MainScreen() {
 		super(null);
 		// TODO Auto-generated constructor stub
