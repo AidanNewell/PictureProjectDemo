a7bfbf2e087a24edc498e8465a583a24e26eb161
Aidan Newell <am.newell@verizon.net>
2017-01-03 13:05:41 -0500

    Here r photos

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -4,10 +4,16 @@ public class MainScreen extends FlexiblePictureExplorer{
 	private String path = "slideshow/";
 	//This is used so I don't have to type it out every time in the three dimensional array
 	
-	
-	private String[][][] images = new String[][][] {{{},{},{}},
-										 	 	   {{},{},{}},
-										 	 	   {{},{},{}}};
+	//Each slideshow of photos is on a different line
+	private final String[][][] images = new String[][][] {{{path+"children.jpg",FINDTHIS,path+"Nassau Hall.jpg",path+"Amtrak.jpg",path+"Plane.jpg",path+"Jane.jpg",path+"TravelCollage.jpg",path+"Cat.jpg",path+"watermelon-touramline.jpg",path+"Littlebrook.PNG",path+"Princeton-Day-School.PNG",path+"Bucks-Rock.jpg",path+"graduation.jpg"},
+													       {path+"school.jpg",path+"Brown.jpg",path+"formula.jpg",path+"IBM.jpg",path+"StonyBrook.jpg",path+"Generic-Diploma.jpg",path+"Temp-ui.jpg"},
+												       	   {path+"maryland-road-map.gif",path+"STSI.jpg",path+"hubble.jpg",path+"JHU.jpg",path+"Schedule.jpg",FINDTHIS,path+"auditorium.jpg"}},
+										 	 	          {{FINDTHIS,FINDTHIS,path+"timonium.png",path+"Matt.jpg",path+"Rachel.jpg",path+"Ellicott.PNG",path+"Guinea Pigs.jpg",path+"centennial.png",path+"Columbia.PNG"},
+												      	   {},
+													       {path+"Gender-Symbol.jpg",path+"Trans-Pride.jpg",path+"boy-and-girl-playing.jpg",path+"Gender-Dysphoria",path+"brain.jpg",path+"Transition.jpg",FINDTHIS,path+"Discrimination.png",COLLAGE}},
+										 	  	          {{},
+													       {},
+													       {}}};
 	
 	
 	
